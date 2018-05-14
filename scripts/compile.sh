f9d559f568ba3cf17c2a523769fc4ed1ae087cb5
James Bracy <waratuman@gmail.com>
2018-05-14 13:56:57 -0700

    Version bump + tests

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -9,3 +9,7 @@ rollup -f cjs -i lib/rrule.js -o builds/rrule.cjs.js
 rollup -f es -i lib/rrule.js -o builds/rrule.es.js
 rollup -n rfc5545-rrule -f iife -i lib/rrule.js -o builds/rrule.iife.js
 rollup -n rfc5545-rrule -f umd -i lib/rrule.js -o builds/rrule.umd.js
+
+rollup -f cjs -i lib/test/rrule-parser-test.js -o builds/test/rrule-parser-test.js
+rollup -f cjs -i lib/test/rrule-test.js -o builds/test/rrule-test.js
+rollup -f cjs -i lib/test/rrule-visitor-test.js -o builds/test/rrule-visitor-test.js

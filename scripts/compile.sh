8e5b774861cdb7e0c4e73f88deedaa7248cbd2a7
James Bracy <waratuman@gmail.com>
2019-09-24 10:07:46 -0700

    Dependency Update.

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -4,12 +4,12 @@ set -e
 
 tsc -p ./src
 
-rollup -u rfc5545-rrule  -f amd -i lib/rrule.js -o builds/rrule.amd.js
-rollup -f cjs -i lib/rrule.js -o builds/rrule.cjs.js
-rollup -f es -i lib/rrule.js -o builds/rrule.es.js
-rollup -n rfc5545-rrule -f iife -i lib/rrule.js -o builds/rrule.iife.js
-rollup -n rfc5545-rrule -f umd -i lib/rrule.js -o builds/rrule.umd.js
+rollup -e nearley,qunit -u rfc5545-rrule  -f amd -i lib/rrule.js -o builds/rrule.amd.js
+rollup -e nearley,qunit -f cjs -i lib/rrule.js -o builds/rrule.cjs.js
+rollup -e nearley,qunit -f es -i lib/rrule.js -o builds/rrule.es.js
+rollup -e nearley,qunit -n rfc5545-rrule -f iife -i lib/rrule.js -o builds/rrule.iife.js
+rollup -e nearley,qunit -n rfc5545-rrule -f umd -i lib/rrule.js -o builds/rrule.umd.js
 
-rollup -f cjs -i lib/test/rrule-parser-test.js -o builds/test/rrule-parser-test.js
-rollup -f cjs -i lib/test/rrule-test.js -o builds/test/rrule-test.js
-rollup -f cjs -i lib/test/rrule-visitor-test.js -o builds/test/rrule-visitor-test.js
+rollup -e nearley,qunit -f cjs -i lib/test/rrule-parser-test.js -o builds/test/rrule-parser-test.js
+rollup -e nearley,qunit -f cjs -i lib/test/rrule-test.js -o builds/test/rrule-test.js
+rollup -e nearley,qunit -f cjs -i lib/test/rrule-visitor-test.js -o builds/test/rrule-visitor-test.js
